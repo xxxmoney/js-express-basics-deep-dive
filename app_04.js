eccee26e2ad44173f0ddf0050beb9dce186777d7
xxxmoney <xxxmoney111@gmail.com>
2026-03-23 17:11:52 +0100

    CORS update

--- app_04.js
+++ app_04.js
@@ -24,8 +24,8 @@
 
 // So, what's the fix?
 // As CORS restriction is BE based, we need to change our Express server
-// Ideally, we would only enable a specific origin (ie our frontend) to access our API
-// In this example, we will enable all - so it's simple
+// Ideally, we want to enable a specific origin (ie our frontend) and methods to access our API
+// Rule of thumb - only allow what's needed
 
 //
 // Enabling the CORS
@@ -53,9 +53,17 @@ const app = express();
 
 // Let's define some global middleware here
 app.use(express.json()); // Classic, we already know this
-app.use(cors()); // THIS is the usage the CORS package - like this, it will enable all origins
-// Fun fact - under the hood, it actually just sets the header 'Access-Control-Allow-Origin' to '*' - which means all origins are allowed
+//app.use(cors()); // THIS is the usage the CORS package - like this, it will enable all origins
+// BUT - because we want to be specific, we will only allow the frontend
+app.use(cors({
+   origin: 'http://127.0.0.1:5500', // Technically the same as http://localhost:5500 - will explain later why we use this one
+   methods: ['GET']
+}));
+// Fun fact - under the hood, it actually sets the header
+// When allowing all - 'Access-Control-Allow-Origin': '*'
+// When allowing specific origin - 'Access-Control-Allow-Origin': 'http://localhost:5500'
 // Without this header, the browser will block the request from different origins - hence the CORS error
+// You can actually see this in the Headers tab
 
 // Great, with this defined, now let's just define some simple endpoint
 app.get('/beep', (request, response) => {
@@ -71,3 +79,12 @@ app.listen(PORT, () => {
 
 // Now, with this setup, the frontend running at http://localhost:5500 should be able to call our API at http://localhost:666 without any CORS error
 // Yipee!
+
+// Regarding the localhost VS 127.0.0.1 difference
+// - They are both technically the same - they both refer to the local machine
+// - But in case of CORS in browser, they are treated as different origins
+// - That's why we need to specify the 127.0.0.1 version
+// - You can actually check the Headers -> Origin of the request - it should be http://127.0.0.1:5500
+// - And because CORS checking basically does string comparison, it sees http://127.0.0.1:5500 different from http://localhost:5500
+
+
